d64d95fbeacc832c03fab4a06e035f1c9aaf96e6
Brandon Davis <wowzerss90@yahoo.com>
2023-04-30 23:15:21 -0400

    Made shooter character class and BP. Added springarm and camera component to character. Added axis binding for player movement.

---
+++ Source/Shooter/Private/Actors/ShooterCharacter.cpp
@@ -0,0 +1,80 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/ShooterCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+// Sets default values
+AShooterCharacter::AShooterCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 300.f;
+	CameraBoom->bUsePawnControlRotation = true;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	// Camera does not rotate relative to arm
+	FollowCamera->bUsePawnControlRotation = false;
+
+}
+
+// Called when the game starts or when spawned
+void AShooterCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AShooterCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Check if the input component is valid
+	check(PlayerInputComponent)
+
+		PlayerInputComponent->BindAxis("MoveForward", this, &AShooterCharacter::MoveForward);
+		PlayerInputComponent->BindAxis("MoveRight", this, &AShooterCharacter::MoveRight);
+
+}
+
+void AShooterCharacter::MoveForward(float Value)
+{
+	if(Controller && (Value != 0.f))
+	{
+		// Find out which way is forward
+		const FRotator Rotation{ Controller->GetControlRotation() };
+		const FRotator YawRotation{ 0.f, Rotation.Yaw, 0.f };
+
+		const FVector Direction{ FRotationMatrix{YawRotation}.GetUnitAxis(EAxis::X) };
+
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AShooterCharacter::MoveRight(float Value)
+{
+	if (Controller && (Value != 0.f))
+	{
+		// Find out which way is forward
+		const FRotator Rotation{ Controller->GetControlRotation() };
+		const FRotator YawRotation{ 0.f, Rotation.Yaw, 0.f };
+
+		const FVector Direction{ FRotationMatrix{YawRotation}.GetUnitAxis(EAxis::Y) };
+
+		AddMovementInput(Direction, Value);
+	}
+}
+
