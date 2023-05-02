4b80cfb0a8f1f3d60872af6e0c873f692e26c118
Brandon Davis <wowzerss90@yahoo.com>
2023-05-02 00:41:44 -0400

    Setup animation class

---
+++ Source/Shooter/Private/Animations/ShooterAnimInstance.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animations/ShooterAnimInstance.h"
+#include "Actors/ShooterCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UShooterAnimInstance::UpdateAnimationProperties(float DeltaTime)
+{
+	if(!ShooterCharacter)
+	{
+		ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
+	}
+
+	if(ShooterCharacter)
+	{
+		// Get the lateral speed of the character from velocity
+
+		FVector Velocity{ ShooterCharacter->GetVelocity() };
+		Velocity.Z = 0;
+
+		Speed = Velocity.Size();
+
+		// Is the character in the air?
+		bIsInAir = ShooterCharacter->GetCharacterMovement()->IsFalling();
+
+		// Is the character accelerating
+		if (ShooterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0)
+		{
+			bIsAccelerating = true;
+		}
+		else
+		{
+			bIsAccelerating = false;
+		}
+	}
+}
+
+void UShooterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
+}
